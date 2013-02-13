1c4669acd9b7fdfb15524363cd556c2ae4f4edba
siegelt.@seas.upenn.edu <taras@taricgroup.com>
2013-02-13 15:53:33 -0500

    BrightBeats
    Created in 48 hour hackathon in Spring 2013.
    Is a three.js visulalization of drum patterns created through a real
    physical drum set.

---
+++ beatdata1.js
@@ -0,0 +1,18 @@
+//Javascript file for Bright Beats Project
+var funArray = new Array();
+funArray[0] = 0;
+funArray[1] = 0;
+funArray[2] = 1;
+funArray[3] = 748;
+funArray[4] = 1;
+funArray[5] = 85;
+funArray[6] = 2;
+funArray[7] = 988;
+funArray[8] = 3;
+funArray[9] = 642;
+funArray[10] = 1;
+funArray[11] = 802;
+funArray[12] = 2;
+funArray[13] = 368;
+funArray[14] = 1;
+funArray[15] = 395;
